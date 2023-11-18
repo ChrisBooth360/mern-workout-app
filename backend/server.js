318106cda00b01f64eed9588aaab65a4166908b0
chrisbooth <chris.booth.ct@gmail.com>
2023-11-18 11:09:15 +0200

    Connections to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // Express app
@@ -8,14 +9,25 @@ const app = express();
 // middleware
 app.use(express.json())
 app.use((req, res, next) => {
-    console.log(req.path, req.method)
-    next()
-})
+    console.log(req.path, req.method);
+    console.log("Request Body:", req.body);
+    next();
+});
+
 
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
-})
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to database. \nListening on port', process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+
