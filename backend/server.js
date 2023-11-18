dfa992ef47c5e63929f1d978d12fc45618c5e33a
chrisbooth <chris.booth.ct@gmail.com>
2023-11-18 19:31:59 +0200

    Added controllers, GET, POST, PATCH and DELETE requests

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(express.json())
 app.use((req, res, next) => {
     console.log(req.path, req.method);
-    console.log("Request Body:", req.body);
     next();
 });
 
