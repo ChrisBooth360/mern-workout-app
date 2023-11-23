fe77fe9616e07f4d06b63de55040beffa06d7b69
chrisbooth <chris.booth.ct@gmail.com>
2023-11-23 09:40:41 +0200

    Setting the initial Auth Status

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -45,7 +45,7 @@ userSchema.statics.signup = async function(email, password) {
 
 }
 
-// Statis login metho
+// Statis login method
 userSchema.statics.login = async function(email, password) {
     
     // Validation
